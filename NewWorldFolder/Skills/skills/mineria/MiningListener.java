5cad3d32dea7dceec6b757b71beee1447daa191b
preytor <prey_lol@hotmail.com>
2013-05-21 19:21:25 +0200

    base mineria

--- NewWorldFolder/Skills/skills/mineria/MiningListener.java
+++ NewWorldFolder/Skills/skills/mineria/MiningListener.java
@@ -24,9 +24,11 @@ import core.skills.SkillPlayer;
 import core.skills.SkillPlayerManager;
 import core.skills.SkillType;
 
+
 public class MiningListener implements Listener {
 
 	static SkillPlayerManager	spm;
+	static ProsManager pm;
 	Logger						log;
 	Plugin						pl;
 
@@ -43,7 +45,7 @@ public class MiningListener implements Listener {
 	    PermissionUser user = PermissionsEx.getUser(event.getPlayer());
 	    Block block = event.getClickedBlock();
 	    SkillPlayer sp = spm.getSkillPlayer(player.getName());
-
+	    
 	    int inHand = player.getItemInHand().getTypeId();
 	    Material material;
 	    if (block == null) {
@@ -57,6 +59,7 @@ public class MiningListener implements Listener {
 	    case LEFT_CLICK_BLOCK:
 	    	if ((inHand == 257) || (inHand == 274) || (inHand == 278) || (inHand == 285)) {
 	    		player.sendMessage("test block");
+
 /*	        	if (!sp.isProspectando()) {
 	          		sp.setProspectando(true);
 	          		player.sendMessage(ChatColor.GREEN + "Te preparas para prospectar la piedra");
@@ -90,49 +93,88 @@ public class MiningListener implements Listener {
 		PermissionUser user = PermissionsEx.getUser(ev.getPlayer());
 		ItemPurityBuilder ipb = new ItemPurityBuilder();
 		Block block = ev.getBlock();
+		String name = player.getName();
 
 		Random r = new Random();
 		int inHand = player.getItemInHand().getTypeId();
 		int r2 = 0;
+		r2 = r.nextInt(100);
 
 		if (block.getType() == Material.STONE){
-			if ((player.getGameMode() == GameMode.SURVIVAL) && ((inHand == 257) || (inHand == 274) || (inHand == 278) || (inHand == 285))) {	
-/*				if (sp.isProspectando()) {
-					player.sendMessage("test romper stone prospectando");
-					sp.setProspectando(false);
-				if (sp.getMineriaLvl() >= 20){
-					if (!sp.isProspectando()) {
-						player.sendMessage(ChatColor.GREEN + "Has prospectado la piedra y te ha salido coal");
+			if ((player.getGameMode() == GameMode.SURVIVAL)){
+				if(pm.isProspecting(name)){
+					if(inHand == 274){ //pico de cobre
+						player.sendMessage("test de prospectar");
+					
+						/* Hacer los checkeos de porcentajes de cada herramienta */
+						
+						if(sp.getLevel(SkillType.Mineria)>=20){
+							
+						}else if(sp.getLevel(SkillType.Mineria)>=30){
+							
+						}else if(sp.getLevel(SkillType.Mineria)>=40){
+							
+						}else if(sp.getLevel(SkillType.Mineria)>=50){
+							
+						}
+					}else if(inHand == 257){	// pico de hierro
+						if(sp.getLevel(SkillType.Mineria)>=20){
+							
+						}else if(sp.getLevel(SkillType.Mineria)>=30){
+							
+						}else if(sp.getLevel(SkillType.Mineria)>=40){
+							
+						}else if(sp.getLevel(SkillType.Mineria)>=50){
+						
+						}else if(sp.getLevel(SkillType.Mineria)>=60){
+							
+						}else if(sp.getLevel(SkillType.Mineria)>=70){
+						
+						}
+					}else if(inHand == 278){  // pico de acero
+						if(sp.getLevel(SkillType.Mineria)>=20){
+							
+						}else if(sp.getLevel(SkillType.Mineria)>=30){
+							
+						}else if(sp.getLevel(SkillType.Mineria)>=40){
+							
+						}else if(sp.getLevel(SkillType.Mineria)>=50){
+						
+						}else if(sp.getLevel(SkillType.Mineria)>=60){
+							
+						}else if(sp.getLevel(SkillType.Mineria)>=70){
+						
+						}
 					}
-				}*/
-			}else{
-				player.sendMessage("test romper stone sin prospectar");
+				}else if(!pm.isProspecting(name)){
+					player.sendMessage("test romper stone sin prospectar");
 
-				ItemStack cobblestone = new ItemStack(4);
+					ItemStack cobblestone = new ItemStack(4);
 
-				if (inHand == 274) {
-					ipb.addDrop(new CustomDrop(cobblestone, sp.getLevel(SkillType.Mineria), 1, 50));
-				}
-				if (inHand == 257) {
-					ipb.addDrop(new CustomDrop(cobblestone, sp.getLevel(SkillType.Mineria), 1, 70));
-				}
-				if (inHand == 278) {
-					ipb.addDrop(new CustomDrop(cobblestone, sp.getLevel(SkillType.Mineria), 1, 80));
-				}
-				if (ipb.getDrops().size() > 0){
-					for (ItemStack is : ipb.getDrops()) {
-						this.pl.getServer().getWorld("world").dropItem(blockLocation, is);
+					if (inHand == 274) {
+						ipb.addDrop(new CustomDrop(cobblestone, sp.getLevel(SkillType.Mineria), 1, 50));
 					}
+					if (inHand == 257) {
+						ipb.addDrop(new CustomDrop(cobblestone, sp.getLevel(SkillType.Mineria), 1, 70));
+					}	
+					if (inHand == 278) {
+						ipb.addDrop(new CustomDrop(cobblestone, sp.getLevel(SkillType.Mineria), 1, 80));
+					}
+					if (ipb.getDrops().size() > 0){
+						for (ItemStack is : ipb.getDrops()) {
+							this.pl.getServer().getWorld("world").dropItem(blockLocation, is);
+						}
+					}
+
+					sp.addExperience(SkillType.Mineria, 200);
+					player.sendMessage("Experiencia en mineria subio en 200 puntos");
+					player.sendMessage("Experiencia en mineria: " + sp.getLevel(SkillType.Mineria));
+					player.sendMessage("Nivel de mineria: " + sp.getLevel(SkillType.Mineria));
+					player.sendMessage("Nivel total: " + sp.getTotalLevel());
+
+					ev.setCancelled(true);
+					ev.getBlock().setTypeId(0);
 				}
-
-				sp.addExperience(SkillType.Mineria, 200);
-				player.sendMessage("Experiencia en mineria subio en 200 puntos");
-				player.sendMessage("Experiencia en mineria: " + sp.getLevel(SkillType.Mineria));
-				player.sendMessage("Nivel de mineria: " + sp.getLevel(SkillType.Mineria));
-				player.sendMessage("Nivel total: " + sp.getTotalLevel());
-
-				ev.setCancelled(true);
-				ev.getBlock().setTypeId(0);
 			}
 		}
 	}
